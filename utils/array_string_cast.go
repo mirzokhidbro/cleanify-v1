7e46666fbafe22bfe453b3232811d8efa1c6ca89
mirzoxid <mirzoxidadxamjanov01@gmail.com>
2024-02-07 10:25:04 +0500

    array cast: remove duplicates

--- utils/array_string_cast.go
+++ utils/array_string_cast.go
@@ -5,7 +5,21 @@ import (
 	"strings"
 )
 
+func removeDuplicates(input []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+
+	for _, entry := range input {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+
 func SetArray(input []string) string {
+	input = removeDuplicates(input)
 	replaced := strings.ReplaceAll(strings.Trim(fmt.Sprint(input), "[]"), " ", ", ")
 	return "{" + replaced + "}"
 }
