9399e869f8d3fbf1eae6ab7af059e2d33b3d1ab9
mirzoxid <mirzoxidadxamjanov01@gmail.com>
2024-02-28 00:48:29 +0500

    ping

--- api/handlers/ping.go
+++ api/handlers/ping.go
@@ -2,12 +2,10 @@ package handlers
 
 import (
 	"bw-erp/api/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) Ping(c *gin.Context) {
-	sec := time.Now().Unix()
-	h.handleResponse(c, http.OK, sec)
+	h.handleResponse(c, http.OK, "Ping!")
 }
