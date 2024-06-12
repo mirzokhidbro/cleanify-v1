0ed06b1bfb4622de3df4d16eaf2f30b796a8d6ce
mirzoxid <mirzoxidadxamjanov01@gmail.com>
2024-06-13 00:54:15 +0500

    fixed

--- storage/postgres/order.go
+++ storage/postgres/order.go
@@ -301,7 +301,7 @@ func (stg *orderRepo) GetDetailedByPrimaryKey(ID int) (models.OrderShowResponse,
 		order.OrderItems = append(order.OrderItems, item)
 	}
 
-	rows, err = stg.db.Query(`select sch.status, u.firstname, u.lastname, sch.created_at from status_change_histories sch inner join users u on u.id = sch.user_id where historyable_type = 'orders' and historyable_id = $1 order by created_at desc`, ID)
+	rows, err = stg.db.Query(`select sch.status, u.firstname, u.lastname, sch.created_at from status_change_histories sch inner join users u on u.id = sch.user_id where historyable_type = 'orders' and historyable_id = $1 order by created_at asc`, ID)
 	if err != nil {
 		return order, err
 	}
