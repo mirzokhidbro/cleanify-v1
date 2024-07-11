6671d9411de995c7a94a166d8e7b044378cea970
mirzoxid <mirzoxidadxamjanov01@gmail.com>
2024-07-11 20:27:35 +0500

    fixed

--- models/user.go
+++ models/user.go
@@ -39,7 +39,7 @@ type UserPermissionByCompany struct {
 
 type UpdateUserRequest struct {
 	ID          string `json:"id" binding:"required"`
-	CompanyID   string `json:"company_id" binding:"required"`
+	// CompanyID   string `json:"company_id" binding:"required"`
 	Firstname   string `json:"firstname"`
 	Lastname    string `json:"lastname"`
 	Permissions []struct {
