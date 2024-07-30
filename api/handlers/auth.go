795a49166c1f7bd620fbb8932109585dcd15fa0c
“mirzoxid” <mirzoxidadxamjanov01@gmail.com>
2024-07-30 20:20:02 +0500

    modified

--- api/handlers/auth.go
+++ api/handlers/auth.go
@@ -39,7 +39,7 @@ func (h *Handler) AuthUser(c *gin.Context) {
 		return
 	}
 
-	h.handleResponse(c, http.OK, response)
+	h.handleResponse(c, http.OK, response.AccessToken)
 }
 
 func (h *Handler) RefreshToken(c *gin.Context) {
