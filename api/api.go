f587524833afbb5799adf024a387bcfa0fafaede
“mirzoxid” <mirzoxidadxamjanov01@gmail.com>
2024-12-23 19:57:43 +0500

    cors

--- api/api.go
+++ api/api.go
@@ -10,10 +10,10 @@ import (
 
 func SetUpRouter(h handlers.Handler, cfg config.Config) (r *gin.Engine) {
 	r = gin.New()
-	
+
 	// CORS middleware should be first
 	r.Use(customCORSMiddleware())
-	
+
 	// Other middleware after CORS
 	r.Use(gin.Logger(), gin.Recovery())
 
@@ -132,7 +132,7 @@ func SetUpRouter(h handlers.Handler, cfg config.Config) (r *gin.Engine) {
 
 func customCORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://prod.yangidunyo.group")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
