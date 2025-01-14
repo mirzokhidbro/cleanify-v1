a8924e262fb72330665df431a709f1a43def9599
“mirzoxid” <mirzoxidadxamjanov01@gmail.com>
2025-01-14 10:10:46 +0500

    orders: notification disabled

--- api/handlers/order.go
+++ api/handlers/order.go
@@ -273,32 +273,32 @@ func (h *Handler) UpdateOrderModel(c *gin.Context) {
 			Status:          order.Status,
 		})
 
-		notificationID, _ := h.Stg.Notification().Create(models.CreateNotificationModel{
-			CompanyID: order.CompanyID,
-			ModelType: "orders",
-			ModelID:   order.ID,
-			Details: models.NotificationDetails{
-				Address: *order.Address,
-				Status:  order.Status,
-				Type:    "status_changed",
-			},
-		})
+		// notificationID, _ := h.Stg.Notification().Create(models.CreateNotificationModel{
+		// 	CompanyID: order.CompanyID,
+		// 	ModelType: "orders",
+		// 	ModelID:   order.ID,
+		// 	Details: models.NotificationDetails{
+		// 		Address: *order.Address,
+		// 		Status:  order.Status,
+		// 		Type:    "status_changed",
+		// 	},
+		// })
 
-		notifications, _ := h.Stg.Notification().GetNotificationsByID(models.GetNotificationsByIDRequest{
-			NotificationID: notificationID,
-		})
+		// notifications, _ := h.Stg.Notification().GetNotificationsByID(models.GetNotificationsByIDRequest{
+		// 	NotificationID: notificationID,
+		// })
 
-		for _, notification := range notifications {
-			unreadCount, err := h.Stg.Notification().GetUnreadNotificationsCount(notification.UserID)
-			if err != nil {
-				log.Printf("Error getting unread notifications count: %v", err)
-				continue
-			}
+		// for _, notification := range notifications {
+		// 	unreadCount, err := h.Stg.Notification().GetUnreadNotificationsCount(notification.UserID)
+		// 	if err != nil {
+		// 		log.Printf("Error getting unread notifications count: %v", err)
+		// 		continue
+		// 	}
 
-			notification.UnreadCount = unreadCount
+		// 	notification.UnreadCount = unreadCount
 
-			utils.GetManager().SendMessage(notification.UserID, notification)
-		}
+		// 	utils.GetManager().SendMessage(notification.UserID, notification)
+		// }
 
 		go func() {
 
